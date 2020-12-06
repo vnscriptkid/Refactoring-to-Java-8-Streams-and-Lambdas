b4de79f734e335950f0985018da57801ab88080f
Thanh Nguyen <vnscriptkid@gmail.com>
2020-12-07 00:56:38 +0700

    Fix ant.bat

--- ant.bat
+++ ant.bat
@@ -17,7 +17,7 @@ REM # KIND, either express or implied.  See the License for the
 REM # specific language governing permissions and limitations
 REM # under the License.
 REM #####################################################################
-
+ECHO "%JAVA_HOME%"
 IF DEFINED JAVA_HOME (
   SET JAVA="%JAVA_HOME%\bin\java"
 ) ELSE (
@@ -27,13 +27,6 @@ IF DEFINED JAVA_HOME (
 SET TOP=%~dp0
 SET LAUNCHER_JAR=
 SET BASE_LIB=%TOP%\framework\base\lib
-SET ANT_LIB=%BASE_LIB%\ant
-FOR %%G IN (%BASE_LIB%\ant-*-ant-launcher.jar) DO SET LAUNCHER_JAR=%%G
-REM ECHO %LAUNCHER_JAR%
-IF [%LAUNCHER_JAR%] == [] (
-  ECHO "Couldn't find ant-launcher.jar"
-) ELSE (      
-  ECHO %JAVA% -jar "%LAUNCHER_JAR%" -lib "%ANT_LIB%" %1 %2 %3 %4 %5 %6
-  %JAVA% -jar "%LAUNCHER_JAR%" -lib "%ANT_LIB%" %1 %2 %3 %4 %5 %6
-)
+SET LAUNCHER_JAR=%BASE_LIB%\ant-1.9.0-ant-launcher.jar
+%JAVA% -jar "%LAUNCHER_JAR%" -lib "%ANT_LIB%" %1 %2 %3 %4 %5 %6
 
